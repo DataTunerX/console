6b01f58f8481421e0fcdac770b8ca80ee5f5912b
zhenghao.zhu <zhenghao.zhu@daocloud.io>
2023-11-14 17:04:16 +0800

    refactor: dataset list

--- src/views/dataset/DatasetList.vue
+++ src/views/dataset/DatasetList.vue
@@ -13,14 +13,12 @@ import DatasetItem from './components/DatasetItem.vue';
 
 const { t } = useI18n();
 
-// 常量定义
 const pageSize = ref(10);
 
 const router = useRouter();
 const namespaceStore = useNamespaceStore();
 const { namespace } = storeToRefs(namespaceStore);
 
-// 数据状态
 const search = ref<{ fuzzy: string[] }>();
 const currentPage = ref(1);
 const datasets = ref<Dataset[]>([]);
@@ -29,15 +27,8 @@ const showData = ref<Dataset[]>([]);
 const datasetToDelete = ref<string>('');
 const isShow = ref(false);
 
-// 创建数据集
-const onCreate = () => {
-  router.push({
-    name: 'DatasetCreate',
-  });
-};
-
 // 加载数据集列表
-const fetchDatasets = async () => {
+const loadDatasets = async () => {
   loading.value = true;
   try {
     const res = await datasetClient.list(namespace.value);
@@ -54,6 +45,29 @@ const fetchDatasets = async () => {
   }
 };
 
+// 显示删除对话框
+const showDialog = (dataset: string) => {
+  isShow.value = true;
+  datasetToDelete.value = dataset;
+};
+
+// 隐藏删除对话框
+const hideDialog = () => {
+  isShow.value = false;
+};
+
+const deleteDataset = async () => {
+  try {
+    await datasetClient.delete(namespace.value, datasetToDelete.value);
+    hideDialog();
+    loadDatasets();
+    nSuccess(t('common.notySuccess', { name: t('common.delete') }));
+  } catch (err) {
+    nError(t('common.notyError', { name: t('common.delete') }), err);
+  }
+};
+
+const onCreate = () => router.push({ name: 'DatasetCreate' });
 const onSearch = () => {
   currentPage.value = 1;
 };
@@ -68,40 +82,11 @@ watchEffect(() => {
   showData.value = filteredData.value.slice(startIndex, endIndex);
 });
 
-// 显示删除对话框
-const showDialog = (dataset: string) => {
-  isShow.value = true;
-  datasetToDelete.value = dataset;
-};
-
-// 隐藏删除对话框
-const hideDialog = () => {
-  isShow.value = false;
-};
-
-// 确认删除数据集
-const confirmDelete = () => {
-  datasetClient
-    .delete(namespace.value, datasetToDelete.value)
-    .then(() => {
-      hideDialog();
-      fetchDatasets();
-      nSuccess(t('common.notySuccess', { name: t('common.delete') }));
-    })
-    .catch((err) => {
-      nError(t('common.notyError', { name: t('common.delete') }), err);
-    });
-};
-
 // 监听命名空间变化，重新加载数据集
 watch(
   () => namespaceStore.namespace,
-  () => {
-    fetchDatasets();
-  },
-  {
-    immediate: true,
-  },
+  loadDatasets,
+  { immediate: true },
 );
 </script>
 
@@ -116,7 +101,7 @@ watch(
       v-model:search="search"
       :search-placeholder="' '"
       :fuzzy="{ key: 'fuzzy', single: true }"
-      @refresh="fetchDatasets"
+      @refresh="loadDatasets"
       @search="onSearch"
     >
       <template #action>
@@ -147,7 +132,7 @@ watch(
     :model-value="isShow"
     header="Basic Dialog"
     @cancel="hideDialog"
-    @confirm="confirmDelete"
+    @confirm="deleteDataset"
   >
     <div class="body">
       <div class="content">
